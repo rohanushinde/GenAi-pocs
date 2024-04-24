3a27991653dd41d96bb1c9c07d83d020762e8cf1
KasturiDhuri <156658816+KasturiDhuri@users.noreply.github.com>
2024-04-24 14:02:36 +0530

    iOS demo.swift

--- components/utils/iOS demot.swift
+++ components/utils/iOS demot.swift
@@ -1,31 +1,28 @@
-import SwiftUI
+import UIKit
+ 
+class ViewController: UIViewController {
 
-struct ContentView: View {
-    var body: some View {
-        NavigationView {
-            VStack {
-                NavigationLink(destination: DetailView()) {
-                    Text("Go to Detail View")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-            }
-            .navigationTitle("Home")
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        // Deprecated API: UIWebView
+
+        let webView = UIWebView(frame: view.bounds)
+
+        view.addSubview(webView)
+
+        // Load a webpage
+
+        if let url = URL(string: "https://www.example.com") {
+
+            let request = URLRequest(url: url)
+
+            webView.loadRequest(request)
+
         }
+
     }
+
 }
 
-struct DetailView: View {
-    var body: some View {
-        Text("This is the Detail View")
-            .navigationTitle("Detail")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
